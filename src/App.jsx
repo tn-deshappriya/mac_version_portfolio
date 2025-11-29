fd88244491dfb731e354da40d0abe7aa1d2b54f3
Mr.No-Hat <thiniranilushandeshappriya@gmail.com>
2025-11-30 04:58:17 +0530

    Start:- set background and Nav bar.

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Navbar from '#components/Navbar'
+import React from 'react'
+
+function App() {
+  return (
+    <main>
+      <Navbar />
+    </main>
+  )
+}
+
+export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,30 @@
+
+import dayjs, { Dayjs } from "dayjs"
+import { navIcons, navLinks } from "#constants"
+
+const Navbar = () => {
+    return (
+        <nav>
+            <div>
+                <img src="/images/logo.svg" alt="logo" />
+                <p className="font-bold">T.N.Deshappriya</p>
+
+                <ul>
+                    {navLinks.map(({ id, name }) => (
+                        <li key={id}><p>{name}</p></li>
+                    ))}
+                </ul>
+            </div>
+            <div>
+                <ul>
+                    {navIcons.map(({ id, img }) => (
+                        <li key={id}><img src={img} className="icon-hover" alt={`icon-${id}`} /></li>
+                    ))}
+                </ul>
+                <time>{dayjs().format("ddd MMM D h:mm A")}</time>
+            </div>
+        </nav >
+    )
+}
+
+export default Navbar
